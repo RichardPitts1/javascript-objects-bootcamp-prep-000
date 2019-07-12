d16df737ecc0afc59dfbf63b06ce3d4193257d39
Richard Pitts <richardepitts@gmail.com>
2019-07-12 20:04:22 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,9 +6,7 @@ var playlist = new Object({
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
-  
 }
-
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
